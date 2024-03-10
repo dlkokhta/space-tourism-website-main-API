fe4e1eda3aa8bf004bbee3f7f938ebb90598c070
dlkokhta <dl.kokhtashvili@gmail.com>
2024-03-10 16:32:06 +0400

    Refactor MongoDB connection string: Replace hardcoded string with environment variables

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    const url = process.env.MONGO_URL!;
+    // const url = process.env.MONGO_URL!;
+    const url = `${process.env.MONGO_PROTOCOL}://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}`;
     mongoose.connect(url);
   } catch (error) {
     console.log(error);
